4b384c17bee5524259ed804aa09aab5e488b478f
Neha O7 <neha@o7tec.com>
2024-09-04 10:57:54 +0530

    Intent , Validations , Nested Layout
    FindviewbyId
    btnClick
    Toast
    Error Enabling
    Explicit Intent

--- app/src/main/java/com/o7services/androidkotlin10_12/MainActivity.kt
+++ app/src/main/java/com/o7services/androidkotlin10_12/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.o7services.androidkotlin10_12
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var btn: Button? = null
+    var edt: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // initialising the var with findviewbyid
+        btn = findViewById(R.id.btnNext)
+        edt = findViewById(R.id.edtValue)
+        //clicklistner
+        btn?.setOnClickListener {
+            //Validation
+            if (edt?.text.toString().isNullOrEmpty()) {
+                //error enabling
+                edt?.error = "Value Required"
+                Toast.makeText(this, "Enter a value", Toast.LENGTH_SHORT).show()
+            } else {
+                //navigation from one screen to another using Explicit Intent
+                var intent = Intent(this, RelativeActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
