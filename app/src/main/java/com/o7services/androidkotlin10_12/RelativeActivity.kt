d5e029b0f467d8cf6065ff0846bfc150f2b78e07
Neha O7 <neha@o7tec.com>
2024-09-06 12:55:01 +0530

    snackbar imageview , image import , bacgroung with image

--- app/src/main/java/com/o7services/androidkotlin10_12/RelativeActivity.kt
+++ app/src/main/java/com/o7services/androidkotlin10_12/RelativeActivity.kt
@@ -1,12 +1,15 @@
 package com.o7services.androidkotlin10_12
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class RelativeActivity : AppCompatActivity() {
+    var btn:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,11 @@ class RelativeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btn=findViewById(R.id.btnClick)
+        btn?.setOnClickListener {
+            //Snackbar Message
+            Snackbar.make(it,"This Is my Snackbar message",Snackbar.LENGTH_SHORT).show()
         }
     }
 }
